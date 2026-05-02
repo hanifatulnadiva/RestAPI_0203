366da6a251674d1b1c81f5a1c771749ca1cb48c9
hanifatulnadiva <161436287+hanifatulnadiva@users.noreply.github.com>
2026-05-02 21:06:35 +0700

    menambahkan integrasi bloc untuk hewan dan button untuk simpan

--- lib/logic/ui/pages/add_hewan_page.dart
+++ lib/logic/ui/pages/add_hewan_page.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:restapi/logic/bloc/hewan/hewan_bloc.dart';
+import 'package:restapi/logic/bloc/hewan/hewan_event.dart';
+import 'package:restapi/logic/bloc/hewan/hewan_state.dart';
 
 class AddHewanPage extends StatefulWidget {
   const AddHewanPage({super.key});
@@ -13,18 +17,29 @@ class _AddHewanPageState extends State<AddHewanPage> {
   final _jenisController=TextEditingController();
   final _tanggalLahirController=TextEditingController();
   final _hargaController=TextEditingController();
-  final _statusController=TextEditingController();
 
   String _selectedStatus ='Tersedia';
-  final List<String>_statusOptions = ['Tersedia', 'Terjual'];
   @override
   void dispose(){
     _namaController.dispose();
     _jenisController.dispose();
     _tanggalLahirController.dispose();
     _hargaController.dispose();
-    _statusController.dispose();
     super.dispose();
+  }
+
+  void _submitForm(){
+    if(_formKey.currentState!.validate()){
+      context.read<HewanBloc>().add(
+        CreateHewan({
+          "nama":_namaController.text,
+          "jenis":_jenisController.text,
+          "tangglLahir":_tanggalLahirController.text,
+          "harga":double.parse(_hargaController.text),
+          "status":_selectedStatus,
+        })
+      );
+    }
   }
 
   @override
@@ -40,156 +55,180 @@ class _AddHewanPageState extends State<AddHewanPage> {
         elevation: 0,
         iconTheme: const IconThemeData(color:Colors.white),
       ),
-      body:Container(
-        width: double.infinity,
-        height: double.infinity,
-        decoration: const BoxDecoration(
-          gradient: LinearGradient(colors: [Color(0xFF1A237E), Color(0xFFAD1457)],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomCenter,
+      body: BlocConsumer <HewanBloc, HewanState>(
+        listener: (context,state){
+           if(state is HewanCreatedSuccess){
+            ScaffoldMessenger.of(context).showSnackBar(
+              const SnackBar(content: Text("Hewan berhasil ditambahkan"),
+              backgroundColor: Colors.green,)
+            );
+            Navigator.pop(context);
+           } 
+           else if(state is HewanError){
+            ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(content: Text(state.message),
+              backgroundColor: Colors.red)
+            );
+           }
+        },
+        builder: (context, state){
+          if(state is HewanLoading){
+            return const Center(child: CircularProgressIndicator());
+          }
+        return Container(
+          width: double.infinity,
+          height: double.infinity,
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(colors: [Color(0xFF1A237E), Color(0xFFAD1457)],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomCenter,
+            ),
           ),
-        ),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Container(
-                padding: const EdgeInsets.all(16),
-                decoration: BoxDecoration(
-                  color: Colors.white.withOpacity(0.15),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Container(
+                  padding: const EdgeInsets.all(16),
+                  decoration: BoxDecoration(
+                    color: Colors.white.withOpacity(0.15),
 
-                  shape: BoxShape.circle,
+                    shape: BoxShape.circle,
+                  ),
+                  child: const Icon(
+                    Icons.pets,
+                    size: 50,
+                    color:Colors.white,
+                  ),
                 ),
-                child: const Icon(
-                  Icons.pets,
-                  size: 50,
-                  color:Colors.white,
+                const SizedBox(height: 20,),
+                Text(
+                  "Lengkapi informasi hewan dibawah ini",
+                  style:TextStyle(
+                    color:Colors.white.withOpacity(0.7),
+                    fontSize: 14,
+                  ),
                 ),
-              ),
-              const SizedBox(height: 20,),
-              Text(
-                "Lengkapi informasi hewan dibawah ini",
-                style:TextStyle(
-                  color:Colors.white.withOpacity(0.7),
-                  fontSize: 14,
-                ),
-              ),
-              const SizedBox(height: 30),
-              _buildGlassTextField(
-                controller:_namaController,
-                hint:"Nama Hewan",
-                icon:Icons.badge_outlined,
-                validator:(value){
-                  if(value==null||value.isEmpty){
-                    return 'nama hewan tidak boleh kosong';
-                  }
-                  if (value.length<2){
-                    return 'nama minimal 2 karakter';
-                  }
-                  return null;
-                },
-              ),
-              const SizedBox(height: 16),
+                const SizedBox(height: 30),
                 _buildGlassTextField(
-                  controller: _jenisController,
-                  hint: "Jenis Hewan (Kucing, Sapi, dll)",
-                  icon: Icons.category_outlined,
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Jenis hewan tidak boleh kosong';
+                  controller:_namaController,
+                  hint:"Nama Hewan",
+                  icon:Icons.badge_outlined,
+                  validator:(value){
+                    if(value==null||value.isEmpty){
+                      return 'nama hewan tidak boleh kosong';
+                    }
+                    if (value.length<2){
+                      return 'nama minimal 2 karakter';
                     }
                     return null;
                   },
                 ),
-                const SizedBox(height: 16,),
-                Row(
-                  children: [
-                    Expanded(
-                      child: _buildGlassTextField(
-                        controller: _tanggalLahirController,
-                        hint: "Tanggal Lahir",
-                        icon: Icons.calendar_today_outlined,
-                        readOnly: true,
-                        onTap: () async {
-                          DateTime today = DateTime.now();
+                const SizedBox(height: 16),
+                  _buildGlassTextField(
+                    controller: _jenisController,
+                    hint: "Jenis Hewan (Kucing, Sapi, dll)",
+                    icon: Icons.category_outlined,
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Jenis hewan tidak boleh kosong';
+                      }
+                      return null;
+                    },
+                  ),
+                  const SizedBox(height: 16,),
+                  Row(
+                    children: [
+                      Expanded(
+                        child: _buildGlassTextField(
+                          controller: _tanggalLahirController,
+                          hint: "Tanggal Lahir",
+                          icon: Icons.calendar_today_outlined,
+                          readOnly: true,
+                          onTap: () async {
+                            DateTime today = DateTime.now();
 
-                          DateTime? pickedDate = await showDatePicker(
-                            context: context,
-                            initialDate: today,
-                            firstDate: DateTime(2000),
-                            lastDate: today,
-                          );
-                          if (pickedDate != null) {
-                          _tanggalLahirController.text =
-                              "${pickedDate.day}/${pickedDate.month}/${pickedDate.year}";
+                            DateTime? pickedDate = await showDatePicker(
+                              context: context,
+                              initialDate: today,
+                              firstDate: DateTime(2000),
+                              lastDate: today,
+                            );
+                            if (pickedDate != null) {
+                            _tanggalLahirController.text =
+                                "${pickedDate.day}/${pickedDate.month}/${pickedDate.year}";
+                            }
+                          },
+                          validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Tanggal lahir wajib diisi';
                           }
-                        },
-                        validator: (value) {
-                        if (value == null || value.isEmpty) {
-                          return 'Tanggal lahir wajib diisi';
-                        }
 
-                        try {
-                          List<String> parts = value.split('/');
-                          DateTime inputDate = DateTime(
-                            int.parse(parts[2]),
-                            int.parse(parts[1]),
-                            int.parse(parts[0]),
-                          );
+                          try {
+                            List<String> parts = value.split('/');
+                            DateTime inputDate = DateTime(
+                              int.parse(parts[2]),
+                              int.parse(parts[1]),
+                              int.parse(parts[0]),
+                            );
 
-                          if (inputDate.isAfter(DateTime.now())) {
-                            return 'Tidak boleh melebihi hari ini';
+                            if (inputDate.isAfter(DateTime.now())) {
+                              return 'Tidak boleh melebihi hari ini';
+                            }
+                          } catch (e) {
+                            return 'Format tanggal tidak valid';
                           }
-                        } catch (e) {
-                          return 'Format tanggal tidak valid';
-                        }
-                        return null;
-                        },
+                          return null;
+                          },
+                        ),
                       ),
-                    ),
-                  ],
-                ),
-                const SizedBox(height: 16),
-                _buildGlassTextField(
-                  controller: _hargaController,
-                  hint: "Harga (Rp)",
-                  icon: Icons.attach_money,
-                  keyboardType: TextInputType.number,
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Harga wajib diisi';
-                    }
-                    if (double.tryParse(value) == null) {
-                      return 'Masukkan angka';
-                    }
-                    if (double.parse(value) <= 0) {
-                      return 'Harga harus lebih dari 0';
-                    }
-                    return null;
-                  },
-                ),
-                const SizedBox(height: 16),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      "Status",
-                      style: TextStyle(color: Colors.white70),
-                    ),
-                    const SizedBox(height: 10),
-                    Row(
-                      children: [
-                        _buildStatusButton('Tersedia'),
-                        const SizedBox(width: 10),
-                        _buildStatusButton('Terjual'),
-                      ],
-                    ),
-                  ],
-                ),
-            ],
+                    ],
+                  ),
+                  const SizedBox(height: 16),
+                  _buildGlassTextField(
+                    controller: _hargaController,
+                    hint: "Harga (Rp)",
+                    icon: Icons.attach_money,
+                    keyboardType: TextInputType.number,
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Harga wajib diisi';
+                      }
+                      if (double.tryParse(value) == null) {
+                        return 'Masukkan angka';
+                      }
+                      if (double.parse(value) <= 0) {
+                        return 'Harga harus lebih dari 0';
+                      }
+                      return null;
+                    },
+                  ),
+                  const SizedBox(height: 16),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Status",
+                        style: TextStyle(color: Colors.white70),
+                      ),
+                      const SizedBox(height: 10),
+                      Row(
+                        children: [
+                          _buildStatusButton('Tersedia'),
+                          const SizedBox(width: 10),
+                          _buildStatusButton('Terjual'),
+                        ],
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 30),
+                  ElevatedButton(onPressed: _submitForm, child: const Text("Simpan")),
+              ],
+            ),
           ),
-        ),
+        );
+        },
       ),
     );
   }
